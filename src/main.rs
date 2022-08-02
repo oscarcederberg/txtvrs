4a08d1c43e1374ca9cdaeb0438223e5b27abfe57
oscarcederberg <os7138ce-s@student.lu.se>
2022-08-02 22:17:57 +0200

    Add help command

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::{env, process};
 
 use regex::Regex;
 
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -10,7 +12,7 @@ fn main() {
             eprintln!("error: no arguments passed.");
             process::exit(1);
         }
-        2 => print_page(&args[1]),
+        2 => parse_command(&args[1]),
         _ => {
             eprintln!("error: too many arguments passed.");
             process::exit(1);
@@ -18,10 +20,32 @@ fn main() {
     }
 }
 
-fn print_page(page: &String)
+fn parse_command(command: &String) {
+    match command.to_lowercase().as_str() {
+        "help" => print_help(),
+        _ => {
+            match command.parse() {
+                Ok(argument) => print_page(argument),
+                _ => {
+                    eprintln!("error: could not parse argument.");
+                    process::exit(1);
+                }
+            }
+        }
+    }
+}
+
+fn print_help() {
+    println!("txtvrs, version {}", VERSION);
+    println!("======================");
+    println!("help\t\tsee this message");
+    println!("<n>\t\tretrieve page n from SVT Text");
+}
+
+fn print_page(page: u32)
 {
     let response = reqwest::blocking::get(
-        "https://www.svt.se/text-tv/webb/".to_owned() + page
+        "https://www.svt.se/text-tv/webb/".to_owned() + &page.to_string()
     )
         .expect("Failed to get page")
         .text()
